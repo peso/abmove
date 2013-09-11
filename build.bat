4874f3a681fecf46b02563437f963c951ff833db
Peer Sommerlund <peso@users.sourceforge.net>
2013-09-11 09:20:36 +0200

    Remove machine-specific scripts

--- build.bat
+++
@@ -1,3 +0,0 @@
-setlocal
-c:\Mingw\bin\mingw32-make %* 2>err.log
-head err.log
--- configure.bat
+++
@@ -1,3 +0,0 @@
-setlocal
-set Path=C:\MinGW\bin;C:\Programmer\Subversion\bin;C:\Programmer\CMake 2.6\bin;C:\Mercurial
-cmake -G "MinGW Makefiles" .
